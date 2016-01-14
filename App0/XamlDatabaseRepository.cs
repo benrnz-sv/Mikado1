491d05c6096a7b9243760abcb96a16abcde910f2
Ben Rees <brees@fraedom.com>
2016-01-14 21:22:02 +1300

    Copy loading code to a new XamlDb class

---
+++ App0/XamlDatabaseRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using Portable.Xaml;
+
+namespace App0
+{
+    public class XamlDatabaseRepository : IDatabaseRepository
+    {
+        private string _fileName;
+
+        public ApplicationDatabase MyData { get; private set; }
+
+        public void Load()
+        {
+            if (!File.Exists(_fileName))
+            {
+                var defaultDatabase = new ApplicationDatabase
+                {
+                    MyInt = 11,
+                    MyTextData = "The quick brown fox jumped over the lazy dog.",
+                    NetWorth = 25.21M,
+                };
+                using (var stream = new FileStream(_fileName, FileMode.CreateNew))
+                {
+                    XamlServices.Save(stream, defaultDatabase);
+                }
+            }
+
+            using (var stream = new FileStream(_fileName, FileMode.Open))
+            {
+                MyData = (ApplicationDatabase)XamlServices.Load(stream);
+            }
+        }
+    }
+}
